1783907921e9e1758d5decef84fe9ae0ae0e12da
Sujan <120612188+sujan-22@users.noreply.github.com>
2024-02-12 16:13:45 -0500

    Update Popular.jsx

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -7,7 +7,7 @@ const Popular = () => {
   const [popularProducts, setPopularProducts] = useState([]);
 
   useEffect(() => {
-    fetch("https://localhost:4000/popularinwomen")
+    fetch("http://localhost:4000/popularinwomen")
       .then((response) => response.json())
       .then((data) => setPopularProducts(data));
   }, []);
