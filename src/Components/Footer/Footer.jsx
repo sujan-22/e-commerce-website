cb378a0c51fa1be4c21f6ed3b082d962dae65dee
Sujan <sujanrokad44@gmail.com>
2024-04-06 00:20:14 -0400

    change footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaCopyright } from "react-icons/fa";
+import { GrGithub, GrMail, GrLink } from "react-icons/gr";
 import "./Footer.css";
 
 const Footer = () => {
@@ -8,8 +8,31 @@ const Footer = () => {
       <div className="footer-copyright">
         <hr />
         <p>
-          Copyright <FaCopyright /> 2024 - All rights reserved.
+          Designed & Developed by Sujan Rokad &copy; 2024 - All rights reserved.
         </p>
+        <div className="social-icons">
+          <a
+            href="https://github.com/sujan-22"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GrGithub className="icon" />
+          </a>
+          <a
+            href="mailto:your.email@example.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GrMail className="icon" />
+          </a>
+          <a
+            href="https://my-portfolio-seven-ebon-10.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GrLink className="icon" />
+          </a>
+        </div>
       </div>
     </div>
   );
